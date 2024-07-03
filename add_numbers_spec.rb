8eb877ef6f2d48852bc2a783e312495ced43f6dc
Namrata Soni <namratasoni111@gmail.com>
2024-07-04 00:07:42 +0530

    Rspec -> returns the sum of numbers separated by \n

--- add_numbers_spec.rb
+++ add_numbers_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe 'add_numbers' do
     expect(AddNumbers.add('5')).to eq(5)
   end
 
-  it 'returns the sum of numbers separated by comma' do
-    expect(AddNumbers.add('1,2,3')).to eq(6)
+  it 'returns the sum of numbers separated by \n' do
+    expect(AddNumbers.add('1\n2\n3')).to eq(6)
   end
   
 end
