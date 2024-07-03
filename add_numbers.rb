faae0d6ae755a8d7ac4cd8e96e7815aa15f9c477
Namrata Soni <namratasoni111@gmail.com>
2024-07-03 23:58:20 +0530

    Rspecs to check empty string and returns 0

--- add_numbers.rb
+++ add_numbers.rb
@@ -1,4 +1,12 @@
 class AddNumbers
-    def self.add
+    def self.add(numbers_string)
+      # Split the input string into an array of numbers
+      numbers = numbers_string.split.map(&:to_i)
+        
+      # Calculate the sum of the numbers
+      sum = numbers.inject(0, :+)
+      
+      # Return the sum
+      sum  
     end
 end
--- add_numbers_spec.rb
+++ add_numbers_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe 'add_numbers' do
   it 'check add method is defined or not?' do
     expect(AddNumbers).to respond_to(:add) 
   end
+
+  it 'returns 0 for an empty string' do
+    expect(AddNumbers.add('')).to eq(0)
+  end
   
 end
