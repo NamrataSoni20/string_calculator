b1afd1e8051749a19b2bd6939fb84bd80bc850af
Namrata Soni <namratasoni111@gmail.com>
2024-07-04 00:27:45 +0530

    Rspec -> throw an exception: if Calling add with a negative number

--- add_numbers.rb
+++ add_numbers.rb
@@ -12,13 +12,21 @@ class AddNumbers
         numbers_string = input_string    
       end
        # Replace custom delimiter with comma and split the numbers
-       numbers = numbers_string.split(/[#{delimiter}\n]/).map(&:to_i)
-        
-       # Calculate the sum of the numbers
-       sum = numbers.inject(0, :+)
+       numbers = numbers_string.split(/[#{delimiter}\n]/)
+       numbers = input_string.scan(/-?\d+/).map(&:to_i)
+       # Filter out negative numbers
+       negative_numbers = numbers.select { |num| num < 0 }
+    
+       if negative_numbers.empty?
+        # Calculate the sum of the numbers
+        sum = numbers.inject(0, :+)
        
-       # Return the sum
-       sum
+        # Return the sum
+        return sum
+       else
+         return "Negative numbers not allowed: #{negative_numbers.join(', ')}"
+       end 
+       
     end 
         
 end
--- add_numbers_spec.rb
+++ add_numbers_spec.rb
@@ -14,8 +14,12 @@ RSpec.describe 'add_numbers' do
     expect(AddNumbers.add('5')).to eq(5)
   end
 
-  it 'returns the sum of numbers separated by \n' do
+  it 'returns the sum of numbers separated by //;\n' do
     expect(AddNumbers.add("//;\n1;\n2;\n3")).to eq(6)
+  end
+
+  it 'throw an exception: if Calling add with a negative number' do
+    expect(AddNumbers.add('-1 2 -3')).to eq("Negative numbers not allowed: -1, -3")
   end
   
 end
